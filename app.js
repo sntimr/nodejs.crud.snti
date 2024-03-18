b7c4bd20499d8b3603c6bed61d804eb0bb15127c
santmartines <smartinezr2102@alumno.ipn.mx>
2024-03-17 20:33:43 -0600

    commit

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const express = require('express');
 const mysql = require('mysql2');
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000
 
 // Configuración de la conexión a la base de datos
 const con = mysql.createConnection({
